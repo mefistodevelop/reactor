f02d91735341623a9fe1cfa151933cdd821b9e03
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-11 22:29:33 +0300

    feat: add Messages component
    create template of component

---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Messages.scss';
+
+function Messages(props) {
+  return (
+    <main className="messages">
+      <h1>messages</h1>
+    </main>
+  );
+}
+
+export default Messages;
