3b35a98804a993b86dbd1dceb84a3a91398c4eed
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-11 22:34:06 +0300

    feat: add Profile component
    create template of component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.scss';
+
+function Profile(props) {
+  return (
+    <main className="profile">
+      <h1>profile page</h1>
+    </main>
+  );
+}
+
+export default Profile;
