9f3fad778d46e4427b42f34bc6d2ccefd1106807
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-12 10:34:43 +0300

    feat: add NavLink and styles for Navbar
    add NavLink for navigation menu items
    add scss styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import './Navbar.scss';
+import { NavLink } from 'react-router-dom';
 
 function Navbar(props) {
   return (
     <div className="navbar">
       <nav className="navbar__navigation">
         <ul className="navbar__navigation-list">
-          <li className="navbar__navigation-item">Profile</li>
-          <li className="navbar__navigation-item">Messages</li>
-          <li className="navbar__navigation-item">News</li>
-          <li className="navbar__navigation-item">Music</li>
-          <li className="navbar__navigation-item">Settings</li>
+          <li className="navbar__navigation-item">
+            <NavLink className={'navbar__navigation-link'} to={'/profile'}>
+              Profile
+            </NavLink>
+          </li>
+          <li className="navbar__navigation-item">
+            <NavLink className={'navbar__navigation-link'} to={'/messages'}>
+              Messages
+            </NavLink>
+          </li>
+          <li className="navbar__navigation-item">
+            <NavLink className={'navbar__navigation-link'} to={'/news'}>
+              News
+            </NavLink>
+          </li>
+          <li className="navbar__navigation-item">
+            <NavLink className={'navbar__navigation-link'} to={'music'}>
+              Music
+            </NavLink>
+          </li>
+          <li className="navbar__navigation-item">
+            <NavLink className={'navbar__navigation-link'} to={'/settings'}>
+              Settings
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
