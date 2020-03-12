1f2e005c7fbc5ae13d9051fc541b09e5a07cf50e
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-12 10:33:31 +0300

    feat: add Route for profile and messages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Messages from './components/Messages/Messages';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
+      <div className="content-wrapper">
+        <Navbar />
+        <div className="content">
+          <Route path={'/profile'} render={() => <Profile />}/>
+          <Route path={'/messages'} render={() => <Messages />}/>
+        </div>
+      </div>
     </div>
   );
 }
