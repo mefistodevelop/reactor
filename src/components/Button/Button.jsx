ec8bd5850f49ef7aa01ff059d19e7ec958564ffb
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-13 09:20:21 +0300

    feat: add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Button.scss';
+
+function Button(props) {
+  return (
+    <button 
+      className="button" 
+      type={ props.type || 'button' } 
+      onClick={ props.onClick || '' }
+    >
+      { props.text }
+    </button>
+  );
+}
+
+export default Button;
