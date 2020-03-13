2b0d4de571cab90426fd8315ee4475a4e11b6106
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-13 12:27:57 +0300

    feat: add functions to add new post and update typed text

--- src/components/Profile/MyPosts/AddPost/AddPost.jsx
+++ src/components/Profile/MyPosts/AddPost/AddPost.jsx
@@ -4,14 +4,37 @@ import Userpic from '../../../Userpic/Userpic';
 import Button from '../../../Button/Button';
 
 function AddPost(props) {
+
+  const newPost = React.createRef();
+
+  const getText = () => {
+    return newPost.current.value;
+  }
+
+  const newPostTextOnChange = () => {
+    const text = getText();
+    props.updateNewPostText(text);
+  }
+
+  const addPost = () => {
+    const text = getText();
+    props.addNewPost(text);
+  }
+
   return (
     <div className="add-post">
       <div className="add-post__userpic">
         <Userpic path={ props.userData.userpic } />
       </div>
       <div className="add-post__form">
-        <textarea className="add-post__field" placeholder="write something" />
-        <Button type="button" text="share" />
+        <textarea
+          className="add-post__field"
+          ref={ newPost }
+          onChange={ newPostTextOnChange }
+          placeholder="write something"
+          value={ props.getNewPostText() }
+        />
+        <Button type="button" text="share" onClick={ addPost } />
       </div>
     </div>
   );
