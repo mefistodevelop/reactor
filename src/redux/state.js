4bf4409479561107469e6e6f27ce72ca75726ecc
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-13 12:22:56 +0300

    feat: add flux rerendering and new api functions
    AddPost should to use flux rerendering while user enter text
    of new post.
    Add new functions for adding new posts to profile page

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+let callSubscriber = () => '';
+
 const user = {
   id: 1,
   name: 'User name',
@@ -28,8 +30,50 @@ const state = {
         likes: 201,
       },
     ],
+    newPostText: '',
   },
 };
 
+const getNewPostText = () => {
+  return state.profilePage.newPostText;
+}
+
+const getPosts = () => {
+  return state.profilePage.posts;
+}
+
+const getCurrentTime = () => {
+  const today = new Date();
+  const minutes = (today.getMinutes() < 10 ? 0 : '') + today.getMinutes();
+  return `${today.getHours()}:${minutes}`;
+}
+
+const updateNewPostText = (newText) => {
+  state.profilePage.newPostText = newText;
+  callSubscriber(state);
+}
+
+const addNewPost = (postText) => {
+  const posts = state.profilePage.posts;
+  const lastId = posts[posts.length - 1].id;
+
+  posts.push({
+    id: lastId + 1,
+    name: user.name,
+    userpic: user.userpic,
+    time: getCurrentTime(),
+    text: postText,
+    likes: 0,
+  });
+
+  state.profilePage.newPostText = '';
+  callSubscriber(state);
+}
+
+const subscribe = (observer) => {
+  callSubscriber = observer;
+}
+
 export default state;
-export { getUser };
+export { getUser, updateNewPostText, subscribe, getNewPostText, getPosts, addNewPost };
+window.state = state;
