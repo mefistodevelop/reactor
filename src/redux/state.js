ac925fbb7a5cccae8c873b4e79fd5a83e7b36088
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-13 09:18:09 +0300

    feat: add user object to state.js and getter
    user object contains user data
    getter returns user data

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,13 @@
+const user = {
+  id: 1,
+  name: 'User name',
+  userpic: 'https://sun9-39.userapi.com/c624318/v624318471/2b0b4/cRkccpbqGdg.jpg',
+};
+
+const getUser = () => {
+  return user;
+}
+
 const state = {
   profilePage: {
     posts: [
@@ -11,8 +21,8 @@ const state = {
       },
       {
         id: 2,
-        name: 'you know my name',
-        userpic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYGgvh223rQXiRrH1k91ftAkPXZ8rUsDYBAi_UyUgyqwGtVRBl',
+        name: user.name,
+        userpic: user.userpic,
         time: 'yesterday',
         text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, error porro nemo libero doloremque beatae accusamus fugiat culpa placeat perspiciatis?',
         likes: 201,
@@ -22,3 +32,4 @@ const state = {
 };
 
 export default state;
+export { getUser };
