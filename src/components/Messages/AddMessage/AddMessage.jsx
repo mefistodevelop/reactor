25c698bfb210be5db9234de67768d4f73c5fe89a
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-14 15:47:35 +0300

    feat: add AddMessage component

---
+++ src/components/Messages/AddMessage/AddMessage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './AddMessage.scss';
+import Button from '../../Button/Button';
+import { updateNewMessageTextActionCreator, addMessageActionCreator } from '../../../redux/state';
+
+function AddMessage(props) {
+
+  const newMessage = React.createRef();
+
+  const onNewMessageChange = () => {
+    const text = newMessage.current.value;
+    props.dispatch(updateNewMessageTextActionCreator(text));
+  }
+
+  const addNewMessage = () => {
+    props.dispatch(addMessageActionCreator());
+  }
+
+  return (
+    <div className="add-message">
+      <textarea 
+        className="add-message__field"
+        placeholder="Text Message"
+        ref={ newMessage }
+        value={ props.getNewMessageText() }
+        onChange={ onNewMessageChange }
+      />
+      <div className="add-message__button">
+        <Button className="button" type="button" text="Send" onClick={ addNewMessage } />
+      </div>
+    </div>
+  );
+}
+
+export default AddMessage;
