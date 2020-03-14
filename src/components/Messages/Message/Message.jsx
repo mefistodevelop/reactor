d45e87f2bc687cdfd92e298e75f65a13354f9b71
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-14 15:46:19 +0300

    feat: add Message component

---
+++ src/components/Messages/Message/Message.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Message.scss';
+import Userpic from '../../Userpic/Userpic';
+
+function Message(props) {
+
+  const messageMod = props.mod ? ' message_'+ props.mod : '';
+  const userpicMod = props.mod ? ' message__userpic_'+ props.mod : '';
+  const bubbleMod = props.mod ? ' message__bubble_'+ props.mod : '';
+
+  return (
+    <div className={`message ${messageMod}`}>
+      <div className={`message__userpic ${userpicMod}`}>
+        <Userpic path={ props.userpic } />
+      </div>
+      
+      <div className={`message__bubble ${bubbleMod}`}>
+        <p className='message__text'>
+          { props.text }
+        </p>
+        <span className='message__time'>
+          { props.time }
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
