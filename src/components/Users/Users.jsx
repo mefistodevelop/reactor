cbc76826ce058d88d99d4e381158635207a6095b
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-03-27 21:56:00 +0300

    refactor: make Users a class component
    make class component to add getUsers  method which allows
    to get users data from server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import './Users.scss';
 import User from './User/User';
+import * as axios from 'axios';
 
-function Users(props) {
-  const users = props.users.map((user) => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+      this.props.setUsers(response.data.items);
+    });
+  }
+  
+  render() {
+    const users = this.props.users.map((user) => {
+      return (
+        <li className="users__item" key={ user.id }>
+          <User
+            name={ user.name }
+            status={ user.status }
+            userpic={ user.userpic }
+            location={ user.location }
+            followed={ user.followed }
+          />
+        </li>
+      );
+    });
+    
     return (
-      <li className="users__item" key={user.id}>
-        <User
-          name={user.name}
-          status={user.status}
-          userpic={user.userpic}
-          location={user.location}
-          followed={user.followed}
-        />
-      </li>
-
+      <section className="users">
+        <ul className="users__list">
+          { users }
+        </ul>
+      </section>
     );
-  });
-  return (
-    <section className="users">
-      <ul className="users__list">
-        {users}
-      </ul>
-    </section>
-  );
+  };
 }
 
 export default Users;
