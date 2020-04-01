136ca905923fc3906d778970fd2e129ae22734b4
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-01 23:02:12 +0300

    refactor: improve code structure

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import { Route } from 'react-router-dom';
 import MessagesContainer from './components/Messages/MessagesContainer';
 import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
-function App(props) {
-
-  const profileData = <Profile />;
-  const messagesData = <MessagesContainer />;
-
+function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <div className="content-wrapper">
         <Navbar />
         <div className="content">
-          <Route path={'/profile'} render={() => profileData} />
-          <Route path={'/messages'} render={() => messagesData} />
-          <Route path={'/users'} render={() => <UsersContainer /> } />
+          <Route path={'/profile'} render={ () => <ProfileContainer /> }/>
+          <Route path={'/messages'} render={ () => <MessagesContainer /> }/>
+          <Route path={'/users'} render={ () => <UsersContainer /> }/>
         </div>
       </div>
     </div>
