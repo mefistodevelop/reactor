0b36a7dfa57df1fb7244310265991cc99b960c7f
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-02 15:27:46 +0300

    feat: add About component
    component is a block with user information

---
+++ src/components/Profile/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.scss';
+
+function About(props) {
+  return (
+    <div className="about">
+      <h3 className="about__name">{ props.profile.fullName }</h3>
+      <p className="about__status">{ props.profile.aboutMe }</p>
+      <div className="about__work">work</div>
+      <div className="about__location">location</div>
+    </div>
+  );
+}
+
+export default About;
