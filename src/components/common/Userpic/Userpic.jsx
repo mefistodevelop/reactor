82d25a31a0c07ed932103d8609de8ee3c4d1ea4c
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-03 22:31:41 +0300

    feat: add default image to Userpick
    show default image if there is no path in props

--- src/components/common/Userpic/Userpic.jsx
+++ src/components/common/Userpic/Userpic.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Userpic.scss';
+import userImage from '../../../assets/images/user-ghost.png';
 
 function Userpic(props) {
+  const path = props.path ? props.path : userImage;
   const mod =(props.size) ?  `userpic__image_${ props.size }` : 'userpic__image_small';
 
   return (
     <div className="userpic">
-      <img src={ props.path } alt="userpic" className={ `userpic__image ${ mod }` }/>
+      <img src={ path } alt="userpic" className={ `userpic__image ${ mod }` }/>
     </div>
   );
 }
