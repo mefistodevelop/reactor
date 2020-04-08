531068f568a21dbdb058c495df4f1da1fabff355
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-08 12:21:59 +0300

    feat: add Login component

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.scss';
+
+function Login(props) {
+  return (
+    <section className="login">
+      <h1 className="login__title">Loginza</h1>
+    </section>
+  );
+}
+
+export default Login;
