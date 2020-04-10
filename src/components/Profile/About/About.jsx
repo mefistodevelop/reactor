f7030897f1f67cad44f9a28bef47aa8ca51d389f
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-10 22:42:00 +0300

    feat: add Status to profile page

--- src/components/Profile/About/About.jsx
+++ src/components/Profile/About/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './About.scss';
+import Status from './Status/Status';
 
 function About(props) {
   return (
     <div className="about">
       <h3 className="about__name">{ props.profile.fullName }</h3>
-      <p className="about__status">{ props.profile.aboutMe }</p>
+      <div className="about__status">
+        <Status text={ props.profile.aboutMe }/>
+      </div>
       <div className="about__work">Job: { props.profile.lookingForAJobDescription }</div>
       <div className="about__location">location</div>
     </div>
