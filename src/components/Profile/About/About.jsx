6d7c1de43e1ace1e92778385529420a19c833426
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-11 15:21:25 +0300

    refactor: improve profile info about user

--- src/components/Profile/About/About.jsx
+++ src/components/Profile/About/About.jsx
@@ -7,9 +7,16 @@ function About(props) {
     <div className="about">
       <h3 className="about__name">{ props.profile.fullName }</h3>
       <div className="about__status">
-        <Status text={ props.profile.aboutMe }/>
+        <Status text={ props.userStatus }/>
       </div>
-      <div className="about__work">Job: { props.profile.lookingForAJobDescription }</div>
+      <div className="about__about-me">
+        <span className="about__info-title">About me: </span>
+        { props.profile.aboutMe }
+      </div>
+      <div className="about__work">
+        <span className="about__info-title">Job: </span>
+        { props.profile.lookingForAJobDescription }
+      </div>
       <div className="about__location">location</div>
     </div>
   );
