64e8f406bbb2955681fd0104e35dccfe241071ae
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-13 13:47:32 +0300

    feat: add LoginForm to login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Login.scss';
+import LoginForm from './LoginForm/LoginForm';
+import { reduxForm } from 'redux-form';
 
 function Login(props) {
+
+  const LoginReduxForm  = reduxForm({ form: 'login' })(LoginForm);
+
+  const onFormSubmit = (formData) => {
+    console.log(formData);
+  };
   return (
     <section className="login">
-      <h1 className="login__title">Loginza</h1>
+      <div className="login__form">
+        <LoginReduxForm onSubmit={ onFormSubmit } />
+      </div>
     </section>
   );
 }
