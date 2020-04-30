49149857930295d4bd9d569a59618f6d5737e68b
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-30 19:39:54 +0300

    refactor: correct App.js code in accordance with style guide rules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.scss';
-import Navbar from './components/Navbar/Navbar';
+import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Navbar from './components/Navbar/Navbar';
 import MessagesContainer from './components/Messages/MessagesContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import { connect } from 'react-redux';
 import { initializeApp } from './redux/appReducer';
 import Spinner from './components/common/Spinner/Spinner';
 
 class App extends React.Component {
-
   componentDidMount = () => {
+    // eslint-disable-next-line
     this.props.initializeApp();
   }
 
   render() {
-    if (!this.props.initialized) {
-      return <div className="spinner"><Spinner size="100" /></div>
+    const { initialized } = this.props;
+
+    if (!initialized) {
+      return <div className="spinner"><Spinner size="100" /></div>;
     }
 
     return (
@@ -29,16 +32,20 @@ class App extends React.Component {
           <Navbar />
           <div className="content">
             <Route exact path="/" render={() => <Login />} />
-            <Route path={'/profile/:userId?'} render={() => <ProfileContainer />} />
-            <Route path={'/messages'} render={() => <MessagesContainer />} />
-            <Route path={'/users'} render={() => <UsersContainer />} />
-            <Route path={'/login'} render={() => <Login />} />
+            <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+            <Route path="/messages" render={() => <MessagesContainer />} />
+            <Route path="/users" render={() => <UsersContainer />} />
+            <Route path="/login" render={() => <Login />} />
           </div>
         </div>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  initialized: PropTypes.bool.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   initialized: state.app.initialized,
