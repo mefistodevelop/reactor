ed2d35b2d20ebf288536e52c1e278e3f2f0a75b9
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-05-13 00:51:26 +0300

    refactor: improve code structure
    remove useless function to create a button
    add link to name
    use propTypes
    improve code style

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,58 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './User.scss';
 import { NavLink } from 'react-router-dom';
 import Userpic from '../../common/Userpic/Userpic';
 
-function User(props) {
-  const getButton = () => {
-    if (props.followed) {
-      return (
-        <button className="button" 
-          disabled={ props.followingInProgress.some((id) => id === props.id) }
-          onClick={ () => props.unfollow(props.id)
-        }>
-          unfollow
-        </button>
-      );
-    } else {
-      return (
-        <button className="button" 
-          disabled={ props.followingInProgress.some((id) => id === props.id) }
-          onClick={ () => props.follow(props.id)
-        }>
-          follow
-        </button>
-      );
-    }
-  }
+function User({
+  followed,
+  followingInProgress,
+  follow,
+  unfollow,
+  id,
+  userpic,
+  name,
+  status,
+}) {
   return (
     <div className="user">
       <div className="user__main-wrapper">
         <div className="user__userpic">
-          <NavLink to={'/profile/' + props.id }>
-            <Userpic path={ props.userpic } />
+          <NavLink to={`/profile/${id}`}>
+            <Userpic path={userpic} />
           </NavLink>
         </div>
         <div className="user__button">
-          { getButton() }
+          <button
+            className="button"
+            type="button"
+            disabled={followingInProgress.some((userId) => userId === id)}
+            onClick={() => (followed ? unfollow(id) : follow(id))}
+          >
+            {followed ? 'unfollow' : 'follow'}
+          </button>
         </div>
       </div>
 
       <div className="user__info">
         <span className="user__name">
-          { props.name }
+          <NavLink className="user__name-link" to={`/profile/${id}`}>
+            {name}
+          </NavLink>
         </span>
         <span className="user__status">
-          { props.status }
+          {status}
         </span>
       </div>
 
       <div className="user__location">
-        <span className="user__country">{ 'props.location.country' }</span>
-        <span className="user__city">{ 'props.location.city' }</span>
+        <span className="user__country">location.country</span>
+        <span className="user__city">location.city</span>
       </div>
     </div>
   );
 }
+
+User.propTypes = {
+  followed: PropTypes.bool.isRequired,
+  followingInProgress: PropTypes.arrayOf(PropTypes.number).isRequired,
+  follow: PropTypes.func.isRequired,
+  unfollow: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  userpic: PropTypes.string,
+  status: PropTypes.string,
+};
+
+User.defaultProps = {
+  userpic: '',
+  status: '',
+};
 
 export default User;
