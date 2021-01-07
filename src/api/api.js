cd8f0344321e3d79c18aa36eb40d9452fe661ff1
Oleg Rudenko <oleg.ru93@yandex.ru>
2021-01-07 14:17:08 +0300

    refactor: make api-key private

--- src/api/api.js
+++ src/api/api.js
@@ -1,46 +1,33 @@
 import * as axios from 'axios';
+import { API_KEY } from '../keys';
 
 const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   withCredentials: true,
   headers: {
-    'API-KEY': '07273082-6b6c-45d4-9373-3d4dc2812a9e',
+    'API-KEY': API_KEY,
   },
 });
 
 export const usersApi = {
   getUsers(page, pageSize) {
-    return (
-      instance
-        .get(`users?page=${ page }&count=${ pageSize }`)
-        .then((response) => response.data)
-    );
+    return instance
+      .get(`users?page=${page}&count=${pageSize}`)
+      .then((response) => response.data);
   },
 
   followUser(id) {
-    return (
-      instance
-        .post(`follow/${ id }`)
-        .then((response) => response.data)
-    );
+    return instance.post(`follow/${id}`).then((response) => response.data);
   },
 
   unfollowUser(id) {
-    return (
-      instance
-        .delete(`follow/${ id }`)
-        .then((response) => response.data)
-    );
+    return instance.delete(`follow/${id}`).then((response) => response.data);
   },
 };
 
 export const profileApi = {
   getUserProfile(userId) {
-    return (
-      instance
-        .get(`profile/${ userId }`)
-        .then((response) => response.data)
-    );
+    return instance.get(`profile/${userId}`).then((response) => response.data);
   },
 
   getUserStatus(userId) {
@@ -54,24 +41,16 @@ export const profileApi = {
 
 export const authApi = {
   getAuthData() {
-    return (
-      instance
-      .get('/auth/me')
-      .then((response) => response.data)
-    );
+    return instance.get('/auth/me').then((response) => response.data);
   },
 
   login(email, password, rememberMe = false) {
-    return (
-      instance
-        .post('/auth/login', { email, password, rememberMe })
-        .then((response) => response.data)
-    );
+    return instance
+      .post('/auth/login', { email, password, rememberMe })
+      .then((response) => response.data);
   },
 
   logout() {
-    return instance
-    .delete('/auth/login')
-    .then((response) => response.data);
-  }
+    return instance.delete('/auth/login').then((response) => response.data);
+  },
 };
